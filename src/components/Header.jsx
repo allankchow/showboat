40e3a3c51425772c6e30acc21ecae7b82936bd9f
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-01-31 22:25:42 -0800

    Add header and nav elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,43 @@
+import Nav from './Nav';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
 
-
     return (
-        <div>Header</div>
+        <header>
+            <div className="logoContainer">
+                <NavLink to="/">
+                    <div>LOGO PLACEHOLDER</div>
+                </NavLink>
+            </div>
+            <Nav />
+        </header>
     )
 }
 
 export default Header;
+
+
+
+// import { NavLink } from 'react-router-dom';
+
+// const Nav = () => {
+
+//     return (
+//         <nav>
+//             <ul>
+//                 {/* Nav links */}
+//                 <li><NavLink to="/">HOME</NavLink></li>
+//                 <li><NavLink to="/my-list">MY LIST</NavLink></li>
+//                 <li><NavLink to="/about">ABOUT</NavLink></li>
+
+//                 {/* Search input */}
+//                 <form className="searchInput">
+//                     <input type="text" id="searchInput" name="searchInput" />
+//                 </form>
+//             </ul>
+//         </nav>
+//     )
+// }
+
+// export default Nav;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,21 @@
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
 
     return (
-        <div>Nav</div>
+        <nav>
+            <ul>
+                {/* Nav links */}
+                <li><NavLink to="/">HOME</NavLink></li>
+                <li><NavLink to="/my-list">MY LIST</NavLink></li>
+                <li><NavLink to="/about">ABOUT</NavLink></li>
+
+                {/* Search input */}
+                <form className="searchInput">
+                    <input type="text" id="searchInput" name="searchInput" />
+                </form>
+            </ul>
+        </nav>
     )
 }
 
