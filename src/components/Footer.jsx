e2f08dff69fa7addd4b6a5ff07d4c293a4afc3a5
Kevan Ngan <kevanngan_@hotmail.com>
2024-02-02 15:00:02 -0800

    Add nav and logo content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import faTwitter from '../assets/x-twitter.svg';
+
+
 
 const Footer = () => {
 
     return (
-        <div>Footer</div>
+        <footer className="footer">
+            <div className="footerLogo">
+                <img src="/src/assets/logo/movie-database-logo-no-text.png" alt="Site Logo" />
+            </div>
+            <div className="footerSocials">
+                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={faTwitter} alt="x-twitter icon" />
+                </a>
+                <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faFacebook} />
+                </a>
+            </div>
+            <div className="footerNav">
+                <a href="#">Home</a>
+                <a href="#">My List</a>
+                <a href="#">About</a>
+            </div>
+            <div className="footerCategory">
+                <a href="#">Popular</a>
+                <a href="#">Now Playing</a>
+                <a href="#">Top Rated</a>
+                <a href="#">Upcoming</a>
+            </div>
+            <div className="footerCopyright">@2023 Showboat</div>
+        </footer>
     )
 }
 
