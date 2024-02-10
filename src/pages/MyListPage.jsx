9f42f0a13ff38c0f630c04c54ec35976e315c696
Kevan Ngan <kevanngan_@hotmail.com>
2024-02-10 14:51:29 -0800

    Implement useEffect hook to initialize myList state from local storage

--- src/pages/MyListPage.jsx
+++ src/pages/MyListPage.jsx
@@ -1,14 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { appTitle } from "../globals/globalVariables";
+import MovieItem from "../components/MovieItem";
 
 const MyListPage = () => {
+    const [myList, setMyList] = useState([]);
 
     useEffect(() => {
-        document.title = `${appTitle} - My List`;
+        document.title = `${appTitle} - My List`;    
+        const storedList = JSON.parse(localStorage.getItem("myList"));
+        if (storedList) {
+            setMyList(storedList);
+        }
     }, []);
 
     return (
-        <div>My List Page</div>
+        <main>
+            <div className="my-list">
+                <h2>MY LIST</h2>
+                <div className="movie-list">
+                    {myList.map(movie => (
+                        <MovieItem key={movie.id} movie={movie} isInMyList={true}/>
+                    ))}
+                </div>
+            </div>
+        </main>
     )
 }
 
